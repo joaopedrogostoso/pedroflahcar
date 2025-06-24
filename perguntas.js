bd0345cecf3ff63b4ad76565c53f7e2a05a88960
Nildinho Junior. <joao.carriel.fernandes@escola.pr.gov.br>
2025-06-24 10:17:07 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biotecnologia',
+    'para que serve a biotecnologia cinza?',
+    'Foca na conservação e recuperação do meio ambiente, especialmente em áreas contaminadas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual as sete maravilhas do mundo?',
+    'Grande Muralha da China, Petra (Jordânia), Cristo Redentor (Brasil), Machu Picchu (Peru), Chichén Itzá (México), Coliseu (Itália) e Taj Mahal (Índia)'
+)
+
+criaCartao(
+    'MATEMATICA',
+    'faça a conta 1999+2000?',
+    'O resultado apresentado foi 3999'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Mas é caracterizada qual conjunção?',
+    'A palavra "mas" é uma conjunção adversativa em português'
+)
